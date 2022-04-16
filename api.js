a9baf591eeff2a2d4517012f503c7ef42f7fd461
agu6692 <figueredoagustin6692@gmail.com>
2022-04-16 14:24:35 -0300

    completando las peticiones de la api

--- api.js
+++ api.js
@@ -1,15 +1,22 @@
 const express= require('express')
 
+const mongoose= require('mongoose')
+
 const user= require('./user.controller')
 
 const app= express()
 
 const port= 3000
 
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://agu6692:UhA9XaWIimIrDHex@cluster0.6ujto.mongodb.net/PedidosDatabase?retryWrites=true&w=majority')
+
+
 app.get("/",user.list)
 app.post("/",user.create)
 app.get("/:id",user.get)
-app.put("/",user.update)
+app.put("/:id",user.update)
 app.patch("/:id",user.update)
 app.delete("/:id",user.destroy)
 
--- user.controller.js
+++ user.controller.js
@@ -1,18 +1,36 @@
+const Users= require('./User')
+
 const User={
-    list: (req,res)=>{
-        res.status(200).send('todas las colecciones')
+    list:async (req,res)=>{
+        const users= await Users.find()
+        res.status(200).send(users)
     },
-    get: (req,res)=>{
-        res.status(200).send('un objeto de la coleccion')
+    get: async(req,res)=>{
+        const {id}= req.params
+        const user= await Users.findOne({_id:id})
+        res.status(200).send(user)
     },
-    create: (req,res)=>{
-        res.status(201).send('creando coleccion')
+    create: async(req,res)=>{
+        
+        const user= new Users(req.body)
+        const savedUser=await user.save()
+        res.status(201).send(savedUser._id)
     },
-    update: (req,res)=>{
-        res.status(204).send('aztualizando')
+    update: async(req,res)=>{
+        const {id} = req.params
+        const user= await Users.findOne({_id:id})
+
+        Object.assign(user,req.body)
+        await user.save()
+        res.sendStatus(204)
     },
-    destroy: (req,res)=>{
-        res.status(204).send('destruyendo coleccion')
+    destroy: async (req,res)=>{
+        const {id} = req.params
+        const user= await Users.findOne({_id:id})
+        if(user){
+            user.remove()
+        }
+        res.sendStatus(204)
     }
 }
 
